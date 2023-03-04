17792d8329f70632045a9c72fd53528b298e5ef8
Mina Gamil Tawfik <70597499+mina442@users.noreply.github.com>
2023-03-04 16:54:07 +0200

    FlipCard-Memory-Game-master3

--- lib/flip_card_game.dart
+++ lib/flip_card_game.dart
@@ -19,7 +19,7 @@ class FlipCardGame extends StatefulWidget {
 class _FlipCardGameState extends State<FlipCardGame> {
   _FlipCardGameState(this.level);
   Level level;
-  bool isBack = true;
+  
   int previousIndex = -1;
   bool flip = false;
   bool start = false;
@@ -27,6 +27,9 @@ class _FlipCardGameState extends State<FlipCardGame> {
   bool? isFinished;
   Timer? timer;
   int? left; 
+  int? tries; 
+int? target; 
+  int? score;
   List<dynamic>? data;
   List<bool>? cardFlips;
  List<GlobalKey<FlipCardState>>? cardstatekeys;
@@ -70,6 +73,10 @@ class _FlipCardGameState extends State<FlipCardGame> {
   cardFlips = getInitialItemState(level);
   cardstatekeys = getCardStateKeys(level);
   left = (data!.length ~/ 2);
+  target = (data!.length *100 ~/ 2);
+
+  tries = 0;
+  score = 0;
   isFinished = false;
   Future.delayed(Duration(seconds: 6),(){
     setState(() {
@@ -91,14 +98,29 @@ class _FlipCardGameState extends State<FlipCardGame> {
   @override
   Widget build(BuildContext context) {
       return isFinished! ? Scaffold(
+        
+
+        appBar: AppBar(
+          backgroundColor: Color(0xff292a3e),
+          title: Text(' ${level.name} level'),
+        elevation:0 ,
+        ),
               backgroundColor: Color(0xff292a3e),
         body: Center(
-          child: GestureDetector(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+               if(level != Level.Easy) GestureDetector(
             onTap: () {
-                  setState(() {
-                    restart();
+              if(level == Level.Medium)setState(() {
+                    Navigator.push(context, MaterialPageRoute(
+                builder:(context) => FlipCardGame(Level.Easy)));
                   });
-                },
+                  else if(level == Level.Hard){
+                    Navigator.push(context, MaterialPageRoute(
+                      builder:(context) => FlipCardGame(Level.Medium)));
+                }},
             child: Container(
               height: 50,
               width: 200,
@@ -108,23 +130,96 @@ class _FlipCardGameState extends State<FlipCardGame> {
                 borderRadius: BorderRadius.circular(24),
               ),
               child: 
-               Text("replay",
+               Text("previous",
                 style: TextStyle(
                   color: Colors.white,
                   fontSize: 17,fontWeight: FontWeight.w500
                 ),),
               ),
           ),
+              SizedBox(height: 20,),
+              GestureDetector(
+                onTap: () {
+                      setState(() {
+                        restart();
+                      });
+                    },
+                child: Container(
+                  height: 50,
+                  width: 200,
+                  alignment: Alignment.center,
+                  decoration: BoxDecoration(
+                    color: Colors.green,
+                    borderRadius: BorderRadius.circular(24),
+                  ),
+                  child: 
+                   Text("replay",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 17,fontWeight: FontWeight.w500
+                    ),),
+                  ),
+              ),
+              SizedBox(height: 20,),
+               if(level != Level.Hard)GestureDetector(
+            onTap: () {
+              if(level == Level.Easy)setState(() {
+                    Navigator.push(context, MaterialPageRoute(
+                builder:(context) => FlipCardGame(Level.Medium)));
+                  });
+                  else if(level == Level.Medium){
+                    Navigator.push(context, MaterialPageRoute(
+                      builder:(context) => FlipCardGame(Level.Hard)));
+                }},
+            child: Container(
+              height: 50,
+              width: 200,
+              alignment: Alignment.center,
+              decoration: BoxDecoration(
+                color: Colors.green,
+                borderRadius: BorderRadius.circular(24),
+              ),
+              child: 
+               Text("next",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 17,fontWeight: FontWeight.w500
+                ),),
+              ),
+          ),
+            ],
+          ),
         ),
         )
       :Scaffold(
+        
+        appBar: AppBar(
+          backgroundColor: Color(0xff292a3e),
+          title: Text(' ${level.name} level'),
+        elevation:0 ,
+        ),
               backgroundColor: Color(0xff292a3e),
           body: SafeArea(
             child: SingleChildScrollView(
               child: Column(
                 children: [
                 Padding(padding: EdgeInsets.all(16.0)
-                ,child: time > 0 ? Text("$time",style:TextStyle(color: Colors.white),): Text("left:$left",style: TextStyle(color: Colors.white)),
+                ,child: time > 0 ? Text("$time",style:TextStyle(color: Colors.white),): 
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Text("left:$left",style: TextStyle(color: Colors.white)),
+                    SizedBox(width: 20,),
+                     Text("score:$score",style: TextStyle(color: Colors.white)),
+ SizedBox(width: 20,),
+                     Text("tries:$tries",style: TextStyle(color: Colors.white)),
+ SizedBox(width: 20,),
+                     Text("target:$target",style: TextStyle(color: Colors.white)),
+
+                  ],
+                
+                ),
                 ),
                   Padding(
                       padding: const EdgeInsets.all(4.0),
@@ -150,14 +245,20 @@ class _FlipCardGameState extends State<FlipCardGame> {
                               cardstatekeys![previousIndex].currentState!.toggleCard();
                               previousIndex =index;
                               cardstatekeys![previousIndex].currentState!.toggleCard();
+                                if(score !> 0)
+                                setState(() {
+                              score = score!-100;
+                            });
+                            setState(() {
+                               tries = tries!+1;});
                               
-                                // Future.delayed(
-                                //   Duration(milliseconds: 160),
-                                // (){
-                                //   setState(() {
-                                //   wait = false;
-                                // });
-                                // });
+                                Future.delayed(
+                                  Duration(milliseconds: 160),
+                                (){
+                                  setState(() {
+                                  wait = false;
+                                });
+                                });
             
                             });
                           }
@@ -166,7 +267,10 @@ class _FlipCardGameState extends State<FlipCardGame> {
                             cardFlips![index] = false ;
                             print(cardFlips);
                             setState(() {
-                              left = left! - 1;
+                              left = left!-1;
+                              score = score!+100;
+                              tries = tries!+1;
+
                             });
                             if(cardFlips!.every((t) => t ==false)){
                               print("won");
@@ -182,7 +286,7 @@ class _FlipCardGameState extends State<FlipCardGame> {
                           }
                         }
                       }
-                                  // setState(() {});
+                                  setState(() {});
 
                     },
                   back: Container(
